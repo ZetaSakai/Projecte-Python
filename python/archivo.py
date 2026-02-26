9aa3737b11672a682d654442cfcad454f2b2645f
Juanillo_Tirabuzon <daniel.garciar@educem.net>
2026-02-26 16:45:21 +0100

    TRaball

--- python/archivo.py
+++ python/archivo.py
@@ -1,1 +1,50 @@
-STFU
+print("És el mes de setembre. L’Institut Tecnològic Vallès ha estrenat una nova aula d’informàtica per al cicle
+de Sistemes Microinformàtics i Xarxes (SMX).
+L’aula, anomenada Aula Zero, disposa de:
+• 24 ordinadors de sobretaula
+• 2 portàtils de suport
+• 1 impressora de xarxa
+• 2 switches
+• 1 punt d’accés WiFi
+• 1 servidor local
+El problema és que tot el material ha arribat de cop i encara no hi ha cap sistema d’inventari ni de
+control de manteniment.
+La direcció del centre us contracta com a equip tècnic junior per desenvolupar una eina interna que
+permeti:
+• Registrar tots els equips
+• Controlar el seu estat
+• Anotar tasques de manteniment
+• Consultar informació ràpidament
+• Generar informes per a la coordinació TIC
+El nom del projecte serà: AulaManager
+El primer dia que arribeu a l’aula, trobeu caixes obertes, cables barrejats i etiquetes sense posar.
+La vostra primera tasca és crear un sistema que permeti:
+• Donar d’alta equips
+• Assignar-los un identificador
+• Guardar la seva informació tècnica
+• Saber en quin estat es troben
+SMX1 - M2: Sistemes Operatius Monolloc
+UF3 - Entrega #1
+Professor: Marc Garcia Morancho Pàg. 3 de 3
+Requisits
+Cada equip ha de tenir:
+• ID automàtic
+• Tipus (PC, portàtil, impressora, switch, AP, servidor)
+• Nom d’equip (ex: PC-AULA0-01)
+• Aula
+• Número de sèrie (únic)
+• Sistema operatiu
+• RAM
+• Disc
+• Estat (operatiu / avariat / reparació / baixa)
+• IP (opcional)
+• MAC (opcional)
+• Observacions
+Situació real
+Un dels ordinadors arriba amb l’etiqueta:
+• HP EliteDesk 800
+• Sèrie: HP99871234
+• Windows 11
+• 16GB RAM
+• 512GB SSD
+L’haureu de registrar correctament al sistema.")
