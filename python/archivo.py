025fe3758437b9eedd3d43b628e8b41c53de5f67
MarcEducem_Tirabuzon <marc.reusj@educem.net>
2026-02-26 16:57:25 +0100

    Update archivo.py

--- python/archivo.py
+++ python/archivo.py
@@ -1,5 +1,17 @@
-print("cuants ordinadors hi han")
-print("a: si")
-print("b: no")
-print("c: potser")
-print("d: perque")
+class Salmon:
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+
+    def swim(self):
+        print(f"{self.name} está nadando río arriba!")
+
+    def eat(self, food):
+        self.weight += food
+        print(f"{self.name} comió {food}kg y ahora pesa {self.weight}kg.")
+
+salmon1 = Salmon("Salmónito", 2, 3.5)
+
+salmon1.swim()
+salmon1.eat(0.5)
